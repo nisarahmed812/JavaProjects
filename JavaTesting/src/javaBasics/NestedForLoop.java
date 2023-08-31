4a7b0cbf86538ffafbe093c0fe1e358ce0282a4a
Nisar Ahmed <nisarahmed812@gmail.com>
2023-08-31 22:35:16 +0530

    31 aug commit 1

--- JavaTesting/src/javaBasics/NestedForLoop.java
+++ JavaTesting/src/javaBasics/NestedForLoop.java
@@ -5,11 +5,11 @@ public class NestedForLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int i=1; i<=4; i++)  //outer loop
+		for (int i=0; i<4; i++)  //outer loop
 		{
 			System.out.println("outer loop started");
 			
-			for (int j=1; j<=4; j++)  //inner loop
+			for (int j=0; j<4; j++)  //inner loop
 			{
 				System.out.println("inner loop");
 				
