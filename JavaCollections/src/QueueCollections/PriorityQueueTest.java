4a6f415b79fcfcb3fe305ef05084f6db42897e45
Nisar Ahmed <nisarahmed812@gmail.com>
2023-09-29 23:31:08 +0530

    29 sep c2

--- JavaCollections/src/QueueCollections/PriorityQueueTest.java
+++ JavaCollections/src/QueueCollections/PriorityQueueTest.java
@@ -1,8 +1,13 @@
 package QueueCollections;
 
+import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class PriorityQueueTest {
+
+	// insertion order preserved
+	// duplicate values are allowed
+	// heterogenios data is allowed in LinkedList
 
 	public static void main(String[] args) {
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
@@ -10,10 +15,31 @@ public class PriorityQueueTest {
 		pq.add(900);
 		pq.add(500);
 		pq.add(100);
+		pq.offer(100);
 
-		System.out.println(pq);
 		System.out.println(pq.size());
-		System.out.println(pq.poll()); // first value
+		System.out.println(pq);
+
+		// get head element
+		System.out.println("--------------get head element------------");
+
+		System.out.println(pq.element()); // if empty NoSuchElementException
+		System.out.println(pq.peek()); // if empty returns null
+
+		// return and remove element from queue
+		System.out.println("--------------return and remove element from queue------------");
+
+		System.out.println(pq.remove());
+		System.out.println(pq);
+
+		System.out.println(pq.poll());
+		System.out.println(pq);
+
+		System.out.println("--------------iterate------------");
+		Iterator<Integer> it = pq.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
 	}
 
 }
