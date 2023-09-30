b20be1ea4bf489beae2428afebca983461b8e07e
Nisar Ahmed <nisarahmed812@gmail.com>
2023-09-30 14:57:32 +0530

    30 sep c1

--- JavaCollections/src/ListCollections/CollectionsUse.java
+++ JavaCollections/src/ListCollections/CollectionsUse.java
@@ -1,4 +1,4 @@
-package JavaMiscellaneous;
+package ListCollections;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- JavaCollections/src/MapCollections/HashMapDemo.java
+++ JavaCollections/src/MapCollections/HashMapDemo.java
@@ -1,4 +1,4 @@
-package javaCollections;
+package MapCollections;
 
 import java.util.HashMap;
 import java.util.Iterator;
