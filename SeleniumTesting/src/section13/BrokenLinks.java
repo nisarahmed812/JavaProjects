af9a755963bc997b3e94b87e3e2e23851a8a41cc
Nisar Ahmed <nisarahmed812@gmail.com>
2023-10-01 17:02:39 +0530

    1 oct c2

--- SeleniumTesting/src/section13/BrokenLinks.java
+++ SeleniumTesting/src/section13/BrokenLinks.java
@@ -6,9 +6,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-import static io.restassured.RestAssured.*;
-import io.restassured.response.Response;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,22 +16,25 @@ public class BrokenLinks {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		
-		//Step 1: is to get all urls tied up to the links using selenium
-		//Java methods will call urls and gets you the status code
-		//if status code >400 then that url is not working i.e link tied up to url is not working
-		
-		String url = driver.findElement(By.xpath("//a[text()='SoapUI']")).getAttribute("href");
-		System.out.println(driver.findElement(By.xpath("//a[text()='SoapUI']")).getAttribute("href"));
-		
-		// Send an HTTP GET request to the URL
-		Response response = RestAssured.get(url);
 
-		// Get and print the status code
-        int statusCode = response.getStatusCode();
-        System.out.println("Status Code: " + statusCode);
-        
-        driver.quit();
+		// Step 1: is to get all urls tied up to the links using selenium
+		// Java methods will call urls and gets you the status code
+		// if status code >400 then that url is not working i.e link tied up to url is
+		// not working
+
+		String urlFetched = driver.findElement(By.xpath("//a[text()='SoapUI']")).getAttribute("href");
+		System.out.println(driver.findElement(By.xpath("//a[text()='SoapUI']")).getAttribute("href"));
+
+		URL url = new URL(urlFetched);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+		int statusCode = connection.getResponseCode();
+
+		System.out.println("Status Code: " + statusCode);
+
+		connection.disconnect();
+
+		driver.quit();
 	}
 
 }
